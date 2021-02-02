eb447a07307954724784d90ccabf90ee64180ddf
Joe Garro <jgarro@datadx.com>
2021-02-02 05:13:15 -0800

    #1372:maybe this will help

--- build.sh
+++ build.sh
@@ -6,12 +6,6 @@ microceliumd=$wd/.microcelium
 
 if [ ! -d $microceliumd ] || [ -z "$(ls -A $microceliumd)" ]
 then
-    dotnet nuget list source | grep -io -P "(?<=\d\.)\s+.+(?=\s)" | while read -r line; do
-        dotnet nuget remove source "$line"
-    done
-
-    dotnet nuget add source "https://pkgs.dev.azure.com/datadx/a3336200-6a09-4f84-bc50-005bbda90364/_packaging/DataDxNuget%40Local/nuget/v3/index.json" -n "DataDxNuget@Local"
-
     dotnet tool install microcelium-fake --tool-path $microceliumd --version 1.* --interactive
 fi
 
