7fbfe36346423577aa117f134332bc170b43924d
Joe Garro <jgarro@datadx.com>
2022-03-20 00:27:27 -0700

    #3833 build issue

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-wd=`pwd`
+wd=$(pwd)
 faked=$wd/.fake
 microceliumd=$wd/.microcelium
 
-if [ ! -d $microceliumd ] || [ -z "$(ls -A $microceliumd)" ]
+if [[ ! -d "$microceliumd" ]]
 then
     dotnet tool install microcelium-fake --tool-path $microceliumd --version 1.* --interactive
 fi
 
-if [ ! -d $faked ] || [ -z "$(ls -A $faked)" ]
+if [[ ! -d "$faked" ]]
 then
     dotnet tool install fake-cli --tool-path $faked --version 5.*
 fi
 
+echo $microceliumd
 
 $microceliumd/microcelium-fake -q
 $faked/fake run build.fsx "$@"
