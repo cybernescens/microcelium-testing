d08c4928dddc3ce54367f2e456fefbdd449e6c96
Joe Garro <jgarro@datadx.com>
2022-03-20 00:42:48 -0700

    #3833 needed updated server in azure-pipelines.yaml

--- Build.fsx
+++ Build.fsx
@@ -1,18 +1,13 @@
 #r "paket:
-nuget JetBrains.dotCover.CommandLineTools
-nuget AWSSDK.S3
-nuget Fake.BuildServer.TeamCity
-nuget Fake.Core.Xml
+nuget Fake.BuildServer.TeamFoundation
 nuget Fake.Core.Target
-nuget Fake.Core.Trace
 nuget Fake.DotNet.Cli
 nuget Fake.DotNet.MSBuild
-nuget Fake.DotNet.NuGet
 nuget Fake.IO.FileSystem
 nuget Fake.IO.Zip
-nuget Fake.Runtime 
+nuget Fake.Runtime
 //"
-#load ".microcelium/lib/Microcelium.fsx"
+#load ".microcelium/lib/microcelium.fsx"
 #load ".fake/build.fsx/intellisense.fsx"
 open Fake.Core
 open Fake.BuildServer
@@ -23,17 +18,9 @@ open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 open Microcelium.Fake
 
-BuildServer.install [ TeamCity.Installer ]
+BuildServer.install [ TeamFoundation.Installer ]
+
 CoreTracing.ensureConsoleListener ()
-
-(* read EnvVar 
-  let myEnvVar = Util.environVarOrDefault ["myEnvVarKey1"; "myEnvVarKey2"] "default value"
-*)
-
-(* setting the version
-  let version = Version.fromVersionIni ()   //looks for a .\Version.ini file
-  let version = Version.fromFile "filepath" //looks for a file @ "filepath"
-*)
 
 let version = Version.fromEnvironment ()
 let versionparts = Version.parts version
